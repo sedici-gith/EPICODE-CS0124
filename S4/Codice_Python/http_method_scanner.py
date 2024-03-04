53a869a636f3640be1b7e68a9aeb64003717b699
sedici-gith <149874494+sedici-gith@users.noreply.github.com>
2024-03-04 15:42:02 +0100

    Add files via upload

---
+++ S4/Codice_Python/http_method_scanner.py
@@ -0,0 +1,32 @@
+import http.client
+def main():           #rende metodo gli input da inserire 
+    IP = input("inserire IP target: ")
+    porta = input("inserire porta target (default: 80): ")
+    percorso = input("inserire percorso (default: /): ")
+    metodi = ['GET', 'POST', 'PUT', 'DELETE', 'HEAD']
+
+    if porta == "":
+        porta = 80
+       
+    if percorso == "":
+        percorso = "/"
+    scansione(IP, porta, percorso, metodi)
+
+def scansione(IP, porta, percorso, metodi):    #rende metodo tutto quello scritto dopo
+	try:
+		for metodo in metodi:
+			connection = http.client.HTTPConnection(IP, porta)   #stabilisce una connessione con il percorso stabilito
+			connection.request(metodo, percorso)    #la connessione richiede che metodo e'attivo su quel percorso specifico
+			risposta = connection.getresponse()   #cattura la risposta dalla connessione
+			if risposta.status < 400:    
+			    print(f"metodo {metodo} abilitato, codice {risposta.status}")
+			else:
+			    print(f"metodo {metodo} disabilitato, codice {risposta.status}")
+		print(IP, "/", porta, "/", percorso)
+	except ConnectionRefusedError:
+		print("connessione fallita")
+		print(IP, "/", porta, "/", percorso)
+    
+if __name__ == "__main__":   #rende modulo tutto il codice
+    main()
+
